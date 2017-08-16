d495f143b1621ed4bc72350b9f0e4ae9a73f54c7
James Dowe <vgkid1@gmail.com>
2017-08-16 14:20:48 -0600

    landing and order page made

---
+++ js/order.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+
+// shopping cart
+var items = [];
+var price = [];
+
+var burgername = document.getElementsById('burgername');
+var burgerprice = document.getElementById('burgerprice');
+var burger = document.getElementById('burger');
+var piename = document.getElementById('piename');
+var pieprice = document.getElementById('pieprice');
+var swinename = document.getElementById('swinename');
+var swineprice = document.getElementById('swineprice');
+
+burger.addEventListener('click', function(){
+alert("Hello world!")
+});
+
+})
